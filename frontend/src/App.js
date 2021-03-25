d8edca83f27556443e6fa6c62080f0d5d258cf5d
Romulo Nunes de Menezes Roriz <rnmroriz@gmail.com>
2021-03-25 22:20:11 +0100

    Created product component for homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import { Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
 	return (
-		<Fragment>
-			<h1> Welcome to ProShop </h1>
-		</Fragment>
+		<Router>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<Route path='/' exact component={HomeScreen} />
+					<Route path='/product/:id' component={ProductScreen} />
+				</Container>
+			</main>
+			<Footer />
+		</Router>
 	);
 };
 
